465f6454aae20d3f52dec8693d5e95dddc69a0c5
ryo-is <ryosuke.izumi62@gmail.com>
2023-11-09 13:07:35 +0900

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Hono } from "hono";
+import apiRoutes from "./api";
+import pageRoutes from "./page";
+
+const app = new Hono();
+
+app.get("/", (c) => c.text("Hello Hono!"));
+
+app.route("/api", apiRoutes);
+
+app.route("/page", pageRoutes);
+
+export default app;
